4dc7eccd2dccde4f204773ca9f705b495310a77d
David Ramos <dave@imaginaryterrain.com>
2014-10-08 18:48:33 -0400

    bring in templates

---
+++ _site/config.rb
@@ -0,0 +1,24 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "django_root/public/assets/css"
+sass_dir = "/sass"
+images_dir = "django_root/public/assets/images"
+javascripts_dir = "django_root/public/assets/js"
+fonts_dir = "django_root/public/assets/fonts"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass django_root/public/assets/sass scss && rm -rf sass && mv scss sass
